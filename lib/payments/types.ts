2068a1cfae68286c4887dbbcbf8910756dff9f88
Kailash Prasad Shah <pratikshah2056@gmail.com>
2026-09-12 21:06:08 +0545

    Include COD in PaymentProvider type definition

--- lib/payments/types.ts
+++ lib/payments/types.ts
@@ -1,4 +1,4 @@
-export type PaymentProvider = "eSewa" | "Khalti" | "MyPay" | "Bank Transfer" | "Cash on Delivery";
+export type PaymentProvider = "eSewa" | "Khalti" | "MyPay" | "Bank Transfer" | "Cash on Delivery" | "COD";
 
 export type PaymentInitiationInput = {
   orderId: string;
